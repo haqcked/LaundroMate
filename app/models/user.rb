8ff4a9c62ce4142b0d3be1c791e6bbb8251a48cc
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-03-08 02:27:49 +0600

    cloudinary is set

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :messages, dependent: :destroy
+
+  has_one_attached :photo
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
