a21bd826338840634721fe9dd5e67c61d3bd9e20
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-03-09 23:08:14 +0600

    added geocoder in the forms for registration (new & edit) for exact address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   has_many :bookings, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :messages, dependent: :destroy
@@ -14,5 +17,4 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :nickname, presence: true
   validates :address, presence: true
-  validate :photo
 end
