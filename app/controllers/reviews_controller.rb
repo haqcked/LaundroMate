24d82a6c2eb2c8840c444fc70ca3bcbabfd188c8
Alex Ryelandt <alexandre.ryelandt@gmail.com>
2023-03-23 18:48:05 +0100

    no change

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,19 +7,36 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    @booking = Booking.find(params[:booking_id])
-    @review = @booking.reviews.build(review_params)
-    @review.user = current_user
+    @review = Review.new(review_params)
     if @review.save
-      redirect_to root_path, notice: "Review succesfully created"
+      redirect_to @review.booking, notice: 'Review successfully created!'
     else
-      render :new
+      redirect_to @review.booking, alert: 'Error creating review'
     end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to @review.booking, notice: 'Review succesfully updated!'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to @review.booking, notice: 'Review succesfully deleted!'
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:rating, :comment)
+    params.require(:review).permit(:rating, :comment, :booking_id, :user_id)
   end
 end
