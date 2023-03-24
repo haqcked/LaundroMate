6c36513b9ab88bd22704b90e68db68715ae40f47
Alex Ryelandt <alexandre.ryelandt@gmail.com>
2023-03-24 21:33:55 +0100

    showing reviews on homepage

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,8 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :comment, presence: true
+
+  def self.highest_rated(limit)
+    order(rating: :desc).limit(limit)
+  end
 end
