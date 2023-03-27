315061953308b89bdd461ba70327bc4069514102
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-03-27 21:39:27 +0600

    edit in booking show, booking index

--- Gemfile
+++ Gemfile
@@ -59,6 +59,7 @@ gem "geocoder"
 gem "rails_admin", "3.0"
 gem 'flatpickr', '~> 2.3', '>= 2.3.5.0'
 
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,9 +3,8 @@ class Booking < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :line_items, dependent: :destroy
 
-  # validates :pickup_date, presence: true
-  # validates :delivery_date, presence: true
-  # validates :total_price, presence: true
+  validates :pickup_date, presence: true
+  validates :delivery_date, presence: true
 
   enum status: { pending: 0, confirmed: 1, completed: 2, cancelled: 3, review: 4 }
 
