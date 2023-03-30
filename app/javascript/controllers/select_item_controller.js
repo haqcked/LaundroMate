83c689dd13ae9619aa706276ee61744b6e82b222
abdulhaq abdullah <haqabdullahov@gmail.com>
2023-03-30 22:18:28 +0600

    added banner, debug booking, edited offcanvas

--- app/javascript/controllers/select_item_controller.js
+++ app/javascript/controllers/select_item_controller.js
@@ -24,6 +24,19 @@ export default class extends Controller {
     if (service == null) {
       return
     }
+
+    // Check if the service is already in the cart
+    const modal = document.getElementById("modal");
+    for (let i = 0; i < modal.children.length; i++) {
+      const childElement = modal.children[i];
+      if (childElement.classList.contains("card-order")) {
+      const existingService = childElement.querySelector("p:nth-child(1) strong").textContent.trim();
+      if (existingService === service) {
+        return;
+      }
+    }
+  }
+
     fetch(url, {
       method: method,
       body: formData,
@@ -33,9 +46,9 @@ export default class extends Controller {
         let subtotal = 0;
         for (let i = 0; i < modal.children.length; i++) {
           const childElement = modal.children[i];
-          if (childElement.classList.contains("card-order")) {
-            const priceElement = childElement.querySelector("p:nth-child(3) strong");
-            const priceValue = priceElement.textContent.trim().split("$")[1];
+          if (childElement.classList.contains("price")) {
+            const priceElement = childElement.querySelector("p:nth-child(1) strong");
+            const priceValue = priceElement.textContent.trim().split("€")[1];
             subtotal += parseInt(priceValue);
           }
         }
@@ -48,11 +61,13 @@ export default class extends Controller {
             "<div class='card-order card p-4 mb-3'>" +
               "<p>Service: <strong>" + `${service}` + "</strong></p>" +
               "<p>Weight: <strong>" + `${weight}` + "</strong></p>" +
-              "<p>Price: <strong>$" + `${price}` + "</strong></p>" +
-              "<form class='button_to' method='post' action='/line_items/31'><input type='hidden' name='_method' value='delete' autocomplete='off'><button class='btn btn-danger' type='submit'>Remove item</button><input type='hidden'></form>" +
-              "</div>"
+              "<div class='d-flex justify-content-between price card-order'>" +
+                "<p>Price: <strong>" + `${price}` + "€</strong></p>" +
+                "<form class='button_to' method='post' action='/line_items/31'><input type='hidden' name='_method' value='delete' autocomplete='off'><button class='btn btn-outline-danger' type='submit'>x</button><input type='hidden'></form>" +
+              "</div>" +
+            "</div>"
           );
-          document.getElementById("subtotal").innerHTML = "Sub Total: $" + `${subtotal}`;
+          document.getElementById("subtotal").innerHTML = "Subtotal: " + `${subtotal}` + ".00€";
         // }
       })
       .catch((error) => console.error(error));
